b0e10e90bd9020ce13c7f6bc4bdc762a90e70a12
Rovshen Annayev <rovshen.dev@gmail.com>
2025-01-22 15:31:31 +0500

    feat(episode 5): style the currently active navigation link

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,10 @@
+@props([
+    'active' => false,
+    'isMobile' => false
+])
+
+<a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 font-medium {{ $isMobile ? 'block text-base' : 'text-sm' }}"
+   aria-current="{{ $active ? 'page' : 'false' }}"
+    {{ $attributes }}>
+    {{ $slot }}
+</a>
