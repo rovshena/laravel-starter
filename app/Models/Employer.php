d17a3393b4cf137842beb8e1592fd91ec2c1cb1c
Rovshen Annayev <rovshen.dev@gmail.com>
2025-03-18 23:12:08 +0500

    feat(episode 11): two key eloquent relationship types

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -2,11 +2,18 @@
 
 namespace App\Models;
 
+use Database\Factories\EmployerFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employer extends Model
 {
-    /** @use HasFactory<\Database\Factories\EmployerFactory> */
+    /** @use HasFactory<EmployerFactory> */
     use HasFactory;
+
+    public function jobs(): HasMany
+    {
+        return $this->hasMany(Job::class);
+    }
 }
